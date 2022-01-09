5a1e38c7ab9213f7eb57e8568cff6bc0dfe5a321
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2022-01-09 00:02:57 -0800

    built dynamic mainpage JS components

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -1,7 +1,15 @@
-const content = document.getElementById("content");
+import { renderMenu } from "./menu";
+import { renderHeading } from "./heading";
+import { renderFolderForm } from "./forms";
 
-const renderMainPage = () => {
-    
+const renderMainPage = (content) => {
+    const menu = renderMenu();
+    const heading = renderHeading("folders");
+    const folderForm = renderFolderForm();
+
+    content.appendChild(menu);
+    content.appendChild(heading);
+    content.appendChild(folderForm);
 }
 
 export { renderMainPage }
