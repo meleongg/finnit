2e1f8f05c71d4133a595548c1e1f9c7879d16745
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2022-01-08 16:51:31 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles/reset.css";
+import "./styles/home-page.css";
+import LogoFull from "./images/logo-full.png";
+import LogoHalf from "./images/logo-half.png"
+
+console.log("hello");
+
+const content = document.getElementById("content");
