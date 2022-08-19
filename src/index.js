bc605f03116b2a5e9b80bf8c5bbaa2ff2fb35e41
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2022-08-19 09:44:18 -0700

    small fixups

--- src/index.js
+++ src/index.js
@@ -4,22 +4,4 @@ import { displayController } from "./components/display";
 import { logicController } from "./components/logic";
 
 displayController.displayHeader();
-
-
-// folders = logicController.getFolders();
-// const defaultFolder = new Folder("Inbox", []);
-
-// let folders;
-
-// if (storageAvailable('localStorage')) {
-//     if (localStorage.getItem('folders')) {
-//         folders = localStorage.getItem('folders');
-//     } else {
-//         localStorage.setItem('folders', folders); 
-//     }
-// } else {
-//     alert("Local Storage Unavailable");
-//     // folders = logicController.getFolders();
-// }
-
 logicController.initializeFolders();
